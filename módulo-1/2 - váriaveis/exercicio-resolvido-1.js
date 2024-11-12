8b0be51082bb81cee238ce13cc201311146eb058
Renan Alves <142599889+BommerDev@users.noreply.github.com>
2024-11-12 15:06:02 -0300

    Delete módulo-1 directory

--- módulo-1/2 - váriaveis/exercicio-resolvido-1.js
+++
@@ -1,9 +0,0 @@
-// 1. Declare uma variável imutavel
-// 2. Armazene o nome da cidade em que você mora
-// 3. Imprima o valor dessa variável
-
-// passo 1 e 2
-const CITY = 'Gama - Brasilia';
-
-// passo 3
-console.log(CITY);
--- módulo-1/2 - váriaveis/exercicios-de-variaveis/jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "node",
-  reporters: ["./custom-test-results-processor"],
-  cache: false,
-};
