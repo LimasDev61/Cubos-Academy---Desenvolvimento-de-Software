89a907dc19f15d65adfc34bef73cc46e33ec2c31
BommerDev <renanalves000@gmail.com>
2025-01-09 09:14:13 -0300

    Novo suit de teste Implementado

--- módulo-3/1-teste-automatizados/_test_/soma.test.js
+++ módulo-3/1-teste-automatizados/_test_/soma.test.js
@@ -2,12 +2,16 @@ const {soma} = require("../soma-jest-teste-require");
 const comparar = require("./comparar")
 const { somaOutput } = require("./output");
 
-describe("Teste de soma com hash", () => {
+describe("Teste de Soma", () => {
   it("Valida que 1 + 2 é igual a 3 usando hash", () => {
-    const a = 3;
-    const b = 0;
-    const resultado = soma(a, b);
+    const resultado = soma(1, 2);
     const corresponde = comparar(resultado, somaOutput.output);
     expect(corresponde).toBeTruthy();
+  });
+
+  it("Verifica o resultado incorreto da soma", () => {
+    const resultado = soma(2, 2);
+    const corresponde = comparar(resultado, somaOutput.output);
+    expect(corresponde).toBeFalsy();
   });
 });
