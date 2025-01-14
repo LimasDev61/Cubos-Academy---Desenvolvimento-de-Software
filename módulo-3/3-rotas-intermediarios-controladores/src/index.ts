c4b8340a2bdec15a89a58af2b1658e8428a81721
BommerDev <renanalves000@gmail.com>
2025-01-14 00:03:40 -0300

    Inicio da configuração do servidor

---
+++ módulo-3/3-rotas-intermediarios-controladores/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
+
+const server: Application = express();
+
+const port = parseInt(process.env.PORT || "3000", 10);
+
+server.get("/", (req: Request, res: Response) => {
+  res.send("Servidor está funcionando normalmente! <3");
+});
+
+server.listen(port, (err?: Error) => {
+  if (err) {
+    console.error(`Erro ao iniciar o servidor: ${err.message}`);
+    process.exit(1);
+  }
+
+  console.log(`O servidor está escutando a porta ${port}`);
+});
+
