7968c5f5af734a790b0834049954d910f7cf1035
BommerDev <renanalves000@gmail.com>
2025-02-09 02:56:05 -0300

    Implementado bd json e pasta utils com leitura e escrita

---
+++ módulo-3/8-Pratica-de-API-Assincronismo-Testes/src/utils/arquivos.ts
@@ -0,0 +1,17 @@
+import fs from 'fs/promises';
+import Link from '../modelos/link';
+import { link } from 'fs';
+
+export const lerDados = async (): Promise<Link[]> => {
+    const dados = await fs.readFile('banco-de-dados.json');
+    const converter = JSON.parse(dados.toString());
+
+    return converter;
+}
+
+export const addDados = async (link: Link): Promise<void> => {
+    const dados = await lerDados();
+    dados.push(link);
+
+    await fs.writeFile('banco-de-dados.json', JSON.stringify(dados, null, "\t"));
+}
