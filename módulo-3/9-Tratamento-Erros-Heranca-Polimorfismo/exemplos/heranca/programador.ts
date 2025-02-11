077136880fc88d5e4d0a1c8ddcb9e8967233efec
BommerDev <renanalves000@gmail.com>
2025-02-11 02:55:53 -0300

    Modifiquei o código programador.ts e o vendedor.ts, para dentro da indentação

--- módulo-3/9-Tratamento-Erros-Heranca-Polimorfismo/exemplos/heranca/programador.ts
+++ módulo-3/9-Tratamento-Erros-Heranca-Polimorfismo/exemplos/heranca/programador.ts
@@ -1,14 +1,14 @@
 import { Funcionario } from "./funcionario";
 
 export class Programador extends Funcionario {
-   private linguagens: string[]
+  private linguagens: string[];
 
-    constructor(nome: string, salario: number, linguagens: string[]) {
-        super(nome, salario);
-        this.linguagens = linguagens;
-    }
+  constructor(nome: string, salario: number, linguagens: string[]) {
+    super(nome, salario);
+    this.linguagens = linguagens;
+  }
 
-    obterLinguagens() {
-        return this.linguagens
-    }
+  obterLinguagens() {
+    return this.linguagens;
+  }
 }
--- módulo-3/9-Tratamento-Erros-Heranca-Polimorfismo/exemplos/heranca/vendedor.ts
+++ módulo-3/9-Tratamento-Erros-Heranca-Polimorfismo/exemplos/heranca/vendedor.ts
@@ -1,18 +1,18 @@
 import { Funcionario } from "./funcionario";
 
 export class Vendedor extends Funcionario {
-    comissao: number;
+  comissao: number;
 
-    constructor(nome: string, salario: number, comissao: number) {
-        super(nome, salario);
-        this.comissao = comissao;
-    }
+  constructor(nome: string, salario: number, comissao: number) {
+    super(nome, salario);
+    this.comissao = comissao;
+  }
 
-    salarioCompleto() {
-        return this.comissao + this.obterSalario(); 
-    }
+  salarioCompleto() {
+    return this.comissao + this.obterSalario();
+  }
 
-    adicionarComissao(valor: number) {
-        this.comissao += valor
-    }
+  adicionarComissao(valor: number) {
+    this.comissao += valor;
+  }
 }
