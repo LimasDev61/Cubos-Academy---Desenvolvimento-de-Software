6df376968ea7c3dcb610dc3ffd51815b18565ff1
BommerDev <renanalves000@gmail.com>
2025-02-12 09:04:09 -0300

    Estudos de throw e dá classe Error

---
+++ módulo-3/9-Tratamento-Erros-Heranca-Polimorfismo/exemplos/heranca/throw-erros.ts
@@ -0,0 +1,12 @@
+try {
+    console.log("Antes de lançar um erro!");
+
+    // throw "Tudo errado";
+    throw new Error("Um erro do javascript")
+} catch (error) {
+    //console.log(error);
+    // Erro tratado
+    if(error instanceof Error) {
+        console.log(error.message);
+    }
+}
