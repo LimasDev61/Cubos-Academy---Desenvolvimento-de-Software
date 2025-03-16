3c06340f11074043607348b1b1e17d82ed74d41d
BommerDev <renanalves000@gmail.com>
2025-03-16 07:06:36 -0300

    conexao.ts configurada

--- módulo-4/7-conexao-nodejs-postgresql/src/conexao.ts
+++ módulo-4/7-conexao-nodejs-postgresql/src/conexao.ts
@@ -7,6 +7,18 @@ export const pool = new Pool({
   host: process.env.PG_HOST,
   port: Number(process.env.PG_PORT),
   database: process.env.PG_DATABASE,
+  // max: 20, // Número máximo de conexões permitidas no pool
 });
+
+// se eu criar com os nomes de o .env com esses nomes:
+// PGUSER=
+// PGPASSWORD=
+// PGHOST=localhost
+// PGPORT=5432
+// PGDATABASE=process.env.USER
+
+// eu consigo apenas criar o arquivo de conexão com o banco de dados, dessa forma:
+// import { Pool } from 'pg'
+// const pool = new Pool()
 
     
