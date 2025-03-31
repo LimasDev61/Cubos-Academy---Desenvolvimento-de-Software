c0e3558e0f3f7c562cb0f37f52de59182bc5fa4f
BommerDev <renanalves000@gmail.com>
2025-03-31 01:16:15 -0300

    rota de cadastro configurada

--- módulo-4/8-autenticacao-criptografia/src/routes.ts
+++ módulo-4/8-autenticacao-criptografia/src/routes.ts
@@ -3,6 +3,4 @@ import CreateUser from "./controllers/create-user";
 
 export const routes = Router();
 
-routes.get("/", (req, res) => {
-    res.send("Hello world");});
 routes.post("/users", new CreateUser().create);
