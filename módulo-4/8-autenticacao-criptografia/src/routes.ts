e586ee621164b56a080277fd414716e15649ab27
BommerDev <renanalves000@gmail.com>
2025-04-01 00:39:12 -0300

    Control de login acrescentado ao sistema, e rota para login

--- módulo-4/8-autenticacao-criptografia/src/routes.ts
+++ módulo-4/8-autenticacao-criptografia/src/routes.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import CreateUser from "./controllers/create-user";
 import { ListUsers } from "./controllers/list-users";
 import { DetailsUser } from "./controllers/details-user";
+import LoginController from "./controllers/login-user";
 
 export const routes = Router();
 
 routes.post("/users", new CreateUser().create);
+routes.post("/login", new LoginController().login);
 routes.get("/users", new ListUsers().list);
 routes.get("/users/:id", new DetailsUser().details);
