a7e28819e512434f1557a0dda07796778acb31ba
BommerDev <renanalves000@gmail.com>
2025-03-31 23:50:57 -0300

    Query para encontrar ID, implementado

---
+++ módulo-4/8-autenticacao-criptografia/src/controllers/details-user.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import UserRepository from "../repositories/user-repository";
+
+export class DetailsUser {
+  async details(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const userRepository = new UserRepository();
+      const user = await userRepository.findById(id);
+
+      if (!user) {
+        res.status(404).json({ message: "Usuário nao encontrado" });
+        return;
+      }
+      res.status(200).json(user);
+      return;
+    } catch (error) {
+      const erro = error as Error;
+      res.status(500).json({ error: erro.message });
+      return;
+    }
+  }
+}
