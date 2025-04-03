c42fe6ad224fe325a02a76d6878c0a1e1d7c1284
BommerDev <renanalves000@gmail.com>
2025-04-03 10:38:04 -0300

    JWT para gerar token, foi implementado na pasta util

--- módulo-4/8-autenticacao-criptografia/src/util/gerar-token.ts
+++ módulo-4/8-autenticacao-criptografia/src/util/gerar-token.ts
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 export function gerarToken(id: string) {
-    if (!process.env.JWT_SECRET) {
-      throw new Error("JWT_SECRET is not defined");
-    }
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
   const token = jwt.sign(
     { id, iat: Math.floor(Date.now() / 1000) },
     process.env.JWT_SECRET as string,
